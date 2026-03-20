62db2d5e6f9e5b97e78043e2fb1fc5548ffceb6f
Raphael Norwitz <rnorwitz@nvidia.com>
2026-03-20 23:44:25 +0200

    criu: rdma: Add uverbs async event fd dump ops
    The ibv_open_device() call opens an async event fd along with the uverbs
    character device, and therefore to support saving/restoring IB device
    contexts we have to expect and save state for these files like we do
    with uverbs character devices.
    
    This change creates the uverbs_async_eventfd_dump_ops much like we did
    with uverbs_dumps_ops and hooks the path which checks for anonymous
    inodes to set ops = uverbs_async_eventfd_dump_ops if the anonymous link
    type is "[infinibandevent]". The only piece of data saved for the uverbs
    async event fd is the kernel restrack ID, so we tweak parse_fdinfo_pid()
    to parse ctxn out of fdinfo like we did for the uverbs character device.
    
    With this change we can successfully run criu on a process with open IB
    device contexts and the dump will succeed.
    
    Signed-off-by: Raphael Norwitz <rnorwitz@nvidia.com>

--- criu/include/rdma.h
+++ criu/include/rdma.h
@@ -2,5 +2,8 @@
 #define __CR_RDMA_H__
 
 extern const struct fdtype_ops uverbs_dump_ops;
+extern const struct fdtype_ops uverbs_async_eventfd_dump_ops;
+
+bool is_async_eventfd(char *link);
 
 #endif /* __CR_RDMA_H__ */
--- criu/rdma.c
+++ criu/rdma.c
@@ -17,6 +17,44 @@
 #undef LOG_PREFIX
 #define LOG_PREFIX "rdma: "
 
+bool is_async_eventfd(char *link)
+{
+	return is_anon_link_type(link, "[infinibandevent]");
+}
+
+static int dump_async_eventfile(int lfd, u32 id, const struct fd_parms *p)
+{
+	UverbsAsyncEvFileEntry uvae = UVERBS_ASYNC_EV_FILE_ENTRY__INIT;
+	FileEntry fe = FILE_ENTRY__INIT;
+	struct cr_img *img;
+
+	uvae.id = id;
+
+	/*
+	 * Like eventfd: anon_inode has no path; do not call dump_one_reg_file()
+	 * (fill_fdlink → anon_inode:… breaks mount lookup in files-reg.c).
+	 */
+	if (parse_fdinfo_pid(p->pid, p->fd, FD_TYPES__UVERBSASYNCFD, &uvae))
+		return -1;
+
+	pr_info("Dumping infinibandevent anon_inode %d with id %#x", lfd, id);
+	if (uvae.has_ctxn)
+		pr_info(" ctxn %u", uvae.ctxn);
+	pr_info("\n");
+
+	fe.type = FD_TYPES__UVERBSASYNCFD;
+	fe.id = uvae.id;
+	fe.uvaefd = &uvae;
+
+	img = img_from_set(glob_imgset, CR_FD_FILES);
+	return pb_write_one(img, &fe, PB_FILE);
+}
+
+const struct fdtype_ops uverbs_async_eventfd_dump_ops = {
+	.type = FD_TYPES__UVERBSASYNCFD,
+	.dump = dump_async_eventfile,
+};
+
 static int dump_uverbsfile(int lfd, u32 id, const struct fd_parms *p)
 {
 	UverbsFileEntry uve = UVERBS_FILE_ENTRY__INIT;
