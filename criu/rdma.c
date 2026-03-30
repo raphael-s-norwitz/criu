7a670eee0dab9a60871508343b6e45cc203cd995
Raphael Norwitz <rnorwitz@nvidia.com>
2026-03-30 17:52:59 +0200

    criu: rdma: Add uverbsfd_cinfo to restore chrdev
    Now that criu dump images contain all the basic information about uverbs
    character devices we can proceed to implement the restore logic.
    
    The restore support is added by defining a collect_image_info for uverbs
    character devices, uverbsfd_cinfo, and adding a case to check for the
    file type FD_TYPES__UVERBSFD in the collect_one_file() path. There is
    also some boilerplate required in the image-desc and magic paths.
    
    The collect_image_info is standard, with collect_one_uverbsfd simply
    collecting the UverbsFileEntry and file_desc_add()ing a file_desc_ops,
    uverbs_desc_ops, which points to the uverbsfd_open() function. Then
    uverbsfd_open() opens the character device with open_reg_by_id() since
    it was saved as a regular file. To activate the character device we must
    issue a GET_CONTEXT uverbs call to the character device. We add a helper
    ib_uverbs_get_context_ioctl() to dispatch ioctl() like rdma-core does
    and call it from uverbsfd_open() once we've opened the character device.
    The ioctl requires a device_id, which we hardcode to RDMA_DRIVER_RXE for
    now.
    
    With this change CRIU will log successfully restoring the character
    device and fail when it hits the uverbs async event fd.
    
    Signed-off-by: Raphael Norwitz <rnorwitz@nvidia.com>

--- criu/rdma.c
+++ criu/rdma.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#include <sys/ioctl.h>
+#include <rdma/rdma_user_ioctl_cmds.h>
+#include <rdma/ib_user_ioctl_cmds.h>
+#include <rdma/ib_user_ioctl_verbs.h>
 
 #include "common/compiler.h"
 #include "imgset.h"
@@ -87,4 +91,85 @@ static int dump_uverbsfile(int lfd, u32 id, const struct fd_parms *p)
 const struct fdtype_ops uverbs_dump_ops = {
 	.type = FD_TYPES__UVERBSFD,
 	.dump = dump_uverbsfile,
+};
+
+struct uverbsfd_file_info {
+	UverbsFileEntry *uvfe;
+	struct file_desc d;
+};
+
+static int
+ib_uverbs_get_context_ioctl(int cmd_fd, uint32_t driver_id)
+{
+	struct {
+		struct ib_uverbs_ioctl_hdr hdr;
+		struct ib_uverbs_attr attrs[2];
+	} buf;
+
+	memset(&buf, 0, sizeof(buf));
+
+	buf.hdr.object_id = UVERBS_OBJECT_DEVICE;
+	buf.hdr.method_id = UVERBS_METHOD_GET_CONTEXT;
+	buf.hdr.driver_id = driver_id;
+	buf.hdr.reserved1 = 0;
+	buf.hdr.reserved2 = 0;
+
+	buf.hdr.num_attrs = 0;
+	buf.hdr.length = sizeof(buf.hdr);
+
+	if (ioctl(cmd_fd, RDMA_VERBS_IOCTL, &buf.hdr) != 0)
+		return -errno;
+
+	return 0;
+}
+
+static int uverbsfd_open(struct file_desc *d, int *new_fd)
+{
+	struct uverbsfd_file_info *ui;
+	int fd, ret;
+
+	ui = container_of(d, struct uverbsfd_file_info, d);
+
+	pr_info("Opening uverbsfd id %#x ctxn %u\n", ui->uvfe->id,
+		ui->uvfe->has_ctxn ? ui->uvfe->ctxn : 0);
+
+	fd = open_reg_by_id(ui->uvfe->id);
+	if (fd < 0)
+		return -1;
+
+	// FIXME: Set correct driver_id
+	ret = ib_uverbs_get_context_ioctl(fd, RDMA_DRIVER_RXE);
+	if (ret)
+		goto out_get_context;
+
+	*new_fd = fd;
+	return 0;
+
+out_get_context:
+	close(fd);
+	return -1;
+}
+
+static struct file_desc_ops uverbs_desc_ops = {
+	.type = FD_TYPES__UVERBSFD,
+	.open = uverbsfd_open,
+};
+
+static int collect_one_uverbsfd(void *o, ProtobufCMessage *base, struct cr_img *i)
+{
+	struct uverbsfd_file_info *ui = o;
+
+	ui->uvfe = pb_msg(base, UverbsFileEntry);
+	file_desc_add(&ui->d, ui->uvfe->id, &uverbs_desc_ops);
+
+	pr_info("Collected uverbsfd ctxn %d\n", ui->uvfe->ctxn);
+
+	return 0;
+}
+
+struct collect_image_info uverbsfd_cinfo = {
+	.fd_type = CR_FD_UVERBS_FILE,
+	.pb_type = PB_UVERBS_FILE,
+	.priv_size = sizeof(struct uverbsfd_file_info),
+	.collect = collect_one_uverbsfd,
 };
