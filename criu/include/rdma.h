b6adc508eb373c3b9aeb0cedc8a35ecb7288d3ab
Raphael Norwitz <rnorwitz@nvidia.com>
2026-03-31 15:21:04 +0200

    criu: rdma: Support opening uverbs async event fds
    Now that uverbs character device fd restore works, the last step to
    support restoring IB device contexts is to create an async event FD
    using the restored uverbs file.
    
    To do so, along with the boilerplate required in the magic header,
    image descriptor paths and case in collect_one_file(), this change adds
    a static array of u32s to track the uverbs character device IDs for each
    context number. The file id of each uverbs character device is set at
    the ctxn of the character device in uverbsfd_open() and then the first
    thing uverbsasyncevfd_open() does is check whether the id value is set.
    If it is unset (i.e. file id = 0), then uverbsasyncevfd_open() returns 1
    to retry in case the character device has not been opened yet. If the id
    is found, the fd_desc for the charater device is retrieved using the
    find_file_desc_raw() helper, which gives a pointer to the chrdev fd. A
    ib_uverbs_alloc_async_event_fd_ioctl() helper is added to issue the
    required uverbs call to create the async event fd and return it to the
    caller. Once again the device_id is hardcoded to RDMA_DRIVER_RXE, which
    will be fixed in subsequent changes.
    
    With this change IB device contexts can be successfully saved and
    restored.
    
    Signed-off-by: Raphael Norwitz <rnorwitz@nvidia.com>

--- criu/include/rdma.h
+++ criu/include/rdma.h
@@ -5,6 +5,7 @@ extern const struct fdtype_ops uverbs_dump_ops;
 extern const struct fdtype_ops uverbs_async_eventfd_dump_ops;
 
 extern struct collect_image_info uverbsfd_cinfo;
+extern struct collect_image_info uverbsasyncevfd_cinfo;
 
 bool is_async_eventfd(char *link);
 
